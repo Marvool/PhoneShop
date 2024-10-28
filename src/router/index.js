e5f234598c7252c6b618c589ca696252c55eaab1
Marvool <antonioaaron0408@gmail.com>
2024-10-28 17:51:50 -0500

    avanze_about

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ const router = Router()
 
 router.get('/', (req, res) => res.render('index',{title: 'Presentación'}))
 router.get('/about', (req, res) => res.render('about',{title: 'Sobre mi persona'}))
-router.get('/studies', (req, res) => res.render('studies',{title: 'Sobre mis estudios'}))
 router.get('/contact', (req, res) => res.render('contact',{title: 'Contactame'}))
 
 export default router
