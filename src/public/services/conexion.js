4930e76476acb028a5e5799fd334eeb8cf8d0f11
Marvool <antonioaaron0408@gmail.com>
2024-10-28 13:30:09 -0500

    Realizar_BaseDeDatos

---
+++ src/public/services/conexion.js
@@ -0,0 +1,23 @@
+import pg from "pg";
+const {Client}= pg;
+
+const config={
+    user: 'curriculumweb_db_user',
+    host: 'dpg-csfst908fa8c73a49an0-a.oregon-postgres.render.com',
+    database: 'curriculumweb_db',
+    password: 'uF2HDJCL09irtzVoRe8I1fORrbas2oV8',
+    port: 5432,
+    ssl:{
+        rejectUnauthorized: false
+    }
+}
+
+async function Conectar(params) {
+    const client= new Client(config)
+    try{
+        await cliente.connect()
+        console.log("Conectado a la base de datos :D")
+    }catch(error){
+        console.log(error)
+    }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import { Router }  from "express"
 const router = Router()
 
 
-router.get('/', (req, res) => res.render('index'))
-router.get('/inicio', (req, res) => res.render('inicio'))
-router.get('/contact', (req, res) => res.render('contact'))
+router.get('/', (req, res) => res.render('index',{title: 'Presentación'}))
+router.get('/about', (req, res) => res.render('about',{title: 'Sobre mi persona'}))
+router.get('/studies', (req, res) => res.render('studies',{title: 'Sobre mis estudios'}))
+router.get('/contact', (req, res) => res.render('contact',{title: 'Contactame'}))
 
 export default router
